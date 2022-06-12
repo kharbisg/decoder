d2e7ba32d6c55d40f12ba714a6bee370e440b53d
kharbisg <kharbisg@gmail.com>
2022-06-12 10:56:30 +0800

    started on views

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const exp = require('constants');
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+app.set('view engine', 'pug');
 
-if (process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
-const exchangeRouter = require('./routes/exchangeRoutes')
+const exchangeRouter = require('./routes/exchangeRoutes');
+
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/exchanges/', exchangeRouter);
 
 module.exports = app;
