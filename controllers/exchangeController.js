647cf9616ad818b367047f3b874f46ecdce6a802
kharbisg <kharbisg@gmail.com>
2022-06-12 12:03:59 +0800

    starting to add css and building frontend

--- controllers/exchangeController.js
+++ controllers/exchangeController.js
@@ -1,13 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
 
-const exchanges = JSON.parse(fs.readFileSync(`${__dirname}/../config_data/supported_exchanges.json`))
+const exchanges = JSON.parse(
+  fs.readFileSync(`${__dirname}/../config_data/supported_exchanges.json`)
+);
 
 exports.getAllExchanges = (req, res) => {
-    res.status(200).json({
-        'status': 'success',
-        'exchanges': exchanges});
+  res.status(200).json({
+    status: 'success',
+    exchanges: exchanges,
+  });
 };
 
 exports.getParsedOutput = (req, res) => {
-    res.status(200).json({"input_data": "", "parsed_data": ""});
+  res.status(200).json({ input_data: '', parsed_data: '' });
 };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("App running on port", port);
+  console.log('App running on port', port);
 });
